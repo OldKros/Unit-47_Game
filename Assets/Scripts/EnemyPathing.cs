07951e360c06cd9c5729a67a17bdad4984d69b7d
Krostik <47256571+OldKros@users.noreply.github.com>
2020-09-12 21:03:07 +0100

    Added Enemy Pathing

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> path;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = path[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPath();
+    }
+
+    private void FollowPath()
+    {
+        if (waypointIndex < path.Count)
+        {
+            var targetPosition = path[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(
+                transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+                waypointIndex++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
